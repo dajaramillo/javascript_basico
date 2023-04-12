324b7196213f2c8775d69c30f458665efdf3cd70
Daniel Jaramillo <dajaramillo@unicauca.edu.co>
2023-04-12 13:29:39 -0500

    archivo objetos

---
+++ objetos.js
@@ -0,0 +1,11 @@
+var automovil = {
+    marca: "Chevrolet",
+    modelo: "Cativa",
+    annio: 2015,
+    infoAuto: function() {
+        console.log("Marca Auto " + this.marca);
+    }
+};
+
+automovil.annio;
+
