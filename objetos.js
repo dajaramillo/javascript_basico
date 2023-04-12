3153e89c33a4663a93781522569d09d65d5bbcf1
Daniel Jaramillo <dajaramillo@unicauca.edu.co>
2023-04-12 13:42:06 -0500

    función autos

--- objetos.js
+++ objetos.js
@@ -1,3 +1,4 @@
+//Objeto de manera manual
 var automovil = {
     marca: "Chevrolet",
     modelo: "Cativa",
@@ -8,4 +9,31 @@ var automovil = {
 };
 
 automovil.annio;
+
+
+//Función constructora
+
+function auto(marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+};
+
+
+var autoNuevo = new auto("Renault","Sandero",2020);
+
+autoNuevo;
+
+//RETO
+var autos = [];
+for(let i = 0 ; i < 3 ; i++){
+  var marca = prompt("Ingresa la marca del auto");
+  var modelo = prompt("Ingresa el modelo del auto");
+  var annio = prompt("Ingresa el año del auto");
+  autos.push(new auto (marca, modelo, annio));
+}
+
+for(let i = 0 ; i < autos.length ; i++){
+  console.log(autos[i]);
+}
 
